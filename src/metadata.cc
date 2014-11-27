3271c88ef9da434598bafef2dcefa578c00fd2c6
homerjam <jameshomer85@gmail.com>
2014-11-27 23:39:30 +0000

    Update metadata.cc

--- src/metadata.cc
+++ src/metadata.cc
@@ -70,13 +70,38 @@ class MetadataWorker : public NanAsyncWorker {
         case ImageType::MAGICK: baton->format = "magick"; break;
         case ImageType::UNKNOWN: break;
       }
+      char *
+      itoa (int value, char *result, int base)
+      {
+        // check that the base if valid
+        if (base < 2 || base > 36) { *result = '\0'; return result; }
+    
+        char* ptr = result, *ptr1 = result, tmp_char;
+        int tmp_value;
+    
+        do {
+            tmp_value = value;
+            value /= base;
+            *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+        } while ( value );
+    
+        // Apply negative sign
+        if (tmp_value < 0) *ptr++ = '-';
+        *ptr-- = '\0';
+        while (ptr1 < ptr) {
+            tmp_char = *ptr;
+            *ptr--= *ptr1;
+            *ptr1++ = tmp_char;
+        }
+        return result;
+      }
       const char *p;
       vips_image_get_string(image, VIPS_META_ICC_NAME, &p);
       // VipsImage attributes
       baton->width = image->Xsize;
       baton->height = image->Ysize;
       baton->space = vips_enum_nick(VIPS_TYPE_INTERPRETATION, image->Type);
-      baton->profile = p.c_str();
+      baton->profile = itoa(p);
       baton->channels = image->Bands;
       baton->hasProfile = HasProfile(image);
       // Derived attributes
