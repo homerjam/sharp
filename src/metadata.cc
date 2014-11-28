1e920dd6f742b451088d9d7b7e61665cfa702313
homerjam <jameshomer85@gmail.com>
2014-11-28 00:10:06 +0000

    Update metadata.cc

--- src/metadata.cc
+++ src/metadata.cc
@@ -76,7 +76,7 @@ class MetadataWorker : public NanAsyncWorker {
       baton->width = image->Xsize;
       baton->height = image->Ysize;
       baton->space = vips_enum_nick(VIPS_TYPE_INTERPRETATION, image->Type);
-      baton->profile = *String::Utf8Value(p);
+      baton->profile = p;
       baton->channels = image->Bands;
       baton->hasProfile = HasProfile(image);
       // Derived attributes
